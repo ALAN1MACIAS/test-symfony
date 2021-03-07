0333dda32913f45dda4ecaa2809910c785a100ab
Alan Guillermo Avila Macias <a.g.a.m.3141335414@gmail.com>
2021-03-07 02:15:00 -0600

    Api of cve to json

--- src/Command/UpdateStockCommand.php
+++ src/Command/UpdateStockCommand.php
@@ -3,9 +3,6 @@
 namespace  App\Command;
 
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputArgument;
-use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Serializer\Serializer;
 use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
 use Symfony\Component\Serializer\Encoder\CsvEncoder;
@@ -20,34 +17,10 @@ class UpdateStockCommand extends Command
     $this->projectDir = $projectDir;
     parent::__construct();
   }
-
-  protected function configure() 
-  {
-    $this->setDescription('Update stock records')
-      ->addArgument('markup', InputArgument::OPTIONAL, 'Porcentage markup', 20)
-      ->addArgument('process_date', InputArgument::OPTIONAL, 'Date of the process', date_create()->format('Y-m-d'));
-  }
-
-  protected function execute(InputInterface $input, OutputInterface $output)
-  {
-    // Convert csv file content into iterable php
-
-    $inputFile = $this->projectDir . '/public/docs/ventas.csv';
-
+  
+  public function getCsvRowsAsArray($processDate){
+    $inputFile = $this->projectDir . '/public/docs/'. $processDate .'.csv';
     $decoder = new Serializer([new ObjectNormalizer()], [new CsvEncoder()]);
-    
-    $rows = $decoder->decode(file_get_contents($inputFile), 'csv');
-
-    dd($rows);
-
-    // Loop over records
-
-    // foreach ($suplierProducts as $suplierProduct){
-
-      // Update if matching found in DB
-
-      // Create new records if matching records not found in the DB
-
-    // }
+    return $decoder->decode(file_get_contents($inputFile), 'csv');
   }
 }
